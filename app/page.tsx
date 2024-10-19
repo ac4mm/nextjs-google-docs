8d43d9339a4b7ecf46c474e44c8f1f51a22bb13d
ac4mm <ac4mm@users.noreply.github.com>
2024-10-19 18:32:55 +0200

    implemented custom fonts, button and edit path

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,62 @@
+"use client"
+
+import {useState} from "react";
+import {adventPro} from '@gds/app/ui/fonts';
+
 export default function Home() {
-  return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-                alt="Google Docs"
-                src="https://upload.wikimedia.org/wikipedia/commons/6/66/Google_Docs_2020_Logo.svg?color=indigo&shade=600"
-                className="mx-auto h-10 w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900">
-              Google Docs
-            </h2>
-          </div>
+    const [username, setUsername] = useState('');
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
-              <div>
-                <label htmlFor="username" className="block text-sm font-medium leading-6 text-white-900">
-                  Username
-                </label>
-                <div className="mt-2">
-                  <input
-                      id="username"
-                      name="username"
-                      type="username"
-                      required
-                      autoComplete="username"
-                      className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
+    async function onSubmit(e) {
+        e.preventDefault();
+        console.log(username);
+    }
+
+    return (
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        alt="Google Docs"
+                        src="https://upload.wikimedia.org/wikipedia/commons/6/66/Google_Docs_2020_Logo.svg?color=indigo&shade=600"
+                        className="mx-auto h-10 w-auto"
+                    />
+                    <h2 className={`${adventPro.className} regular mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900`}>
+                        Google Docs
+                    </h2>
                 </div>
-              </div>
 
-              <div>
-                <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Join
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </>
-  );
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form action="#" className="space-y-6" onSubmit={onSubmit}>
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-white-900">
+                                Username
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="username"
+                                    name="username"
+                                    type="username"
+                                    required
+                                    autoComplete="username"
+                                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    onInput={(e) => setUsername(e.target?.value)}
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Join
+                            </button>
+                        </div>
+
+                        {/* <Chat />*/}
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Advent_Pro} from 'next/font/google';
+
+export const adventPro = Advent_Pro({ subsets: ['latin'] });
+
